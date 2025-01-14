244be98f7d3c96f7bbea57bd3f5ef66f300f3f37
Alexandros Papafragakis <alexpapafragakis7@gmail.com>
2025-01-14 22:50:06 +0200

    m

--- TESTING/test2.cpp
+++ TESTING/test2.cpp
@@ -1,12 +1,29 @@
 #include "JSONlang.h"
+#include <iostream>
 
-PROGRAM_BEGIN
-    // Dimiourgia arxikon dedomenon JSON
+int main() {
+    
+// Define emptyObj json with empty object
+    JSON(emptyObj) = OBJECT({});
+// Define book json with an object containing data for a book
+    JSON(book) = OBJECT({
+        {KEY(title), STRING("Gone Girl")},
+        {KEY(published), NUMBER(2012)},
+        {KEY(type), STRING("Thriller")},
+        {KEY(author), OBJECT({
+            {KEY(first_name), STRING("GILLIAN")},
+            {KEY(surname), STRING("FLYNN")},
+            {KEY(age), NUMBER(45)}
+        })}
+    });
+
+// Define week_temperatures json with an array of numbers
     JSON(week_temperatures) = ARRAY({
         NUMBER(20), NUMBER(19.5), NUMBER(19), NUMBER(20),
         NUMBER(19), NUMBER(18.5), NUMBER(19)
     });
 
+// Define students json with an array of objects representing students
     JSON(students) = ARRAY({
         OBJECT({
             {KEY(name), STRING("Nikos Nikolaou")},
@@ -18,48 +35,43 @@ PROGRAM_BEGIN
         })
     });
 
-    JSON(book) = OBJECT({
-        {KEY(title), STRING("Gone Girl")},
-        {KEY(published), NUMBER(2012)},
-        {KEY(type), STRING("Thriller")},
-        {KEY(author), OBJECT({
-            {KEY(first_name), STRING("GILLIAN")},
-            {KEY(last_name), STRING("FLYNN")},
-            {KEY(age), NUMBER(45)}
-        })}
-    });
+// Change 3rd day temperature from 19 to 22
+    SET(week_temperatures)[2] ASSIGN NUMBER(22);
 
-    // --- SET kai ASSIGN ---
-    std::cout << "Testing SET and ASSIGN..." << std::endl;
-    // Allagi timis stis week_temperatures
-    SET(week_temperatures)[2] ASSIGN NUMBER(22);  // Allagi thermokrasias 3is meras se 22
-    // Prostiki email ston proto fititi
-    SET(students)[0]["email"] ASSIGN STRING("nikos@university.edu");
-    // Allagi titlou vivliou
-    SET(book)["title"] ASSIGN STRING("Gone Girl: Revised Edition");
-    std::cout << "After SET and ASSIGN:" << std::endl;
+// Add email address for 1st student
+    SET(students)[0]["e-mail"] ASSIGN STRING("csd444@csd.uoc.gr");
+
+// Assign new object in emptyObj json
+    SET(emptyObj) ASSIGN OBJECT({ std::make_pair(std::string("a"), STRING("alpha")) });
+    
+    // Test APPEND functionality
+    try {
+        std::cout << "Appending to week_temperatures" << std::endl;
+        SET(week_temperatures).APPEND(NUMBER(23), NUMBER(22), NUMBER(200)); // appends values to week_temperatures
+        std::cout << "Appending to students[0][\"grades\"]" << std::endl;
+        std::cout << "Type of students[0][\"grades\"]: " << TYPE_OF(ACCESS(students)[0]["grades"]) << std::endl;
+        SET(students)[0]["grades"].APPEND(OBJECT({KEY(hy255), NUMBER(99)})); // appends a grade for course hy255
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "Week temperatures after appending: ";
     PRINT(week_temperatures);
+
+    std::cout << "Students after appending grade: ";
     PRINT(students);
+
+    // Test ERASE functionality
+    try {
+        ERASE(ACCESS(book)["author"]["age"]); // removes age from author object of book
+        ERASE(ACCESS(book)["type"]);          // removes type of book
+        ERASE(book);                          // removes all book data, now book is an empty object
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "Book after erasing: ";
     PRINT(book);
 
-    // --- APPEND ---
-    std::cout << "Appending to week_temperatures" << std::endl;
-    SET(week_temperatures).APPEND(NUMBER(23), NUMBER(22), NUMBER(20)); // appends values to week_temperatures
-    std::cout << "Appending to students[0][\"grades\"]" << std::endl;
-    std::cout << "Type of students[0][\"grades\"]: " << TYPE_OF(ACCESS(students)[0]["grades"]) << std::endl;
-    SET(students)[0]["grades"].APPEND(OBJECT({KEY(hy255), NUMBER(9)})); // appends a grade for course hy255
-
-    // --- ERASE ---
-    std::cout << "Testing ERASE..." << std::endl;
-    // Diagrafi tou pediou "type" apo to vivlio
-    ERASE(ACCESS(book)["type"]);
-    // Diagrafi tis telefteas thermokrasias ston pinaka
-    ERASE(ACCESS(week_temperatures)[SIZE_OF(week_temperatures) - 1]);
-    // Diagrafi oloklirou tou pinaka grades gia ton proto fititi
-    ERASE(ACCESS(students)[0]["grades"]);
-    std::cout << "After ERASE:" << std::endl;
-    PRINT(week_temperatures);
-    PRINT(students);
-    PRINT(book);
-
-PROGRAM_END
+    return 0;
+}
